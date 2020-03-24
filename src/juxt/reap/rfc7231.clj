d240288b9551d708011831773e2eeaafed6d0693
Malcolm Sparks <mal@juxt.pro>
2020-03-24 17:23:09 +0000

    Add skeletal support for ABNF in RFC 7231
    This is to demonstrate the use of parser combinators to parse a
    reasonably complex ABNF production (the possible values of the HTTP
    Accept header).

---
+++ src/juxt/reap/rfc7231.clj
@@ -0,0 +1,351 @@
+;; Copyright © 2020, JUXT LTD.
+
+(ns juxt.reap.rfc7231
+  (:refer-clojure :exclude [type])
+  (:require
+   [juxt.reap.regex :as re]
+   [juxt.reap.parse :as p]
+   [juxt.reap.rfc7230 :as rfc7230 :refer [OWS token]]
+   [juxt.reap.rfc5234 :as rfc5234 :refer [DIGIT]]))
+
+(defn- common-parameter
+  "A shared implementation of parameter parsing that can support
+  different patterns. Patterns must yield at least 3 groups (name,
+  value and, optionally, quoted-string)."
+  [pattern _]
+  (fn [matcher]
+    (.usePattern matcher pattern)
+    (when (.lookingAt matcher)
+      (let [[_ n v qs] (re-groups matcher)]
+        (re/advance-and-return
+         matcher
+         (apply conj {:name n}
+                (cond v [[:value v]]
+                      qs [[:value (rfc7230/unescape-quoted-string qs)]
+                          [:raw-value (str "\"" qs "\"")]])))))))
+
+(defn parameter
+  "Matches a parameter and returns a map containing :name and :value
+  entries. If the value is in a quoted string, this matcher extracts
+  the content inside the quoted-string to return as the value."
+  ([] (parameter {}))
+  ([opts]
+   (common-parameter
+    (re-pattern
+     (re/re-compose
+      "(%s)=(?:(%s)|(?:%s))"
+      token token rfc7230/quoted-string))
+    opts)))
+
+(defn optional-parameter
+  "Matches an optional parameter and returns a map containing :name
+  and :value entries. If the value is in a quoted string, this matcher
+  extracts the content inside the quoted-string to return as the
+  value."
+  ([] (optional-parameter {}))
+  ([opts]
+   (common-parameter
+    (re-pattern
+     (re/re-compose
+      "(%s)(?:=(?:(%s)|(?:%s)))?"
+      token token rfc7230/quoted-string))
+    opts)))
+
+
+
+
+;; token = <token, see [RFC7230], Section 3.2.6>
+
+;; subtype = token
+(def ^String subtype token)
+
+;; type = token
+(def ^String type token)
+
+
+;; Accept-Charset = *( "," OWS ) ( ( charset / "*" ) [ weight ] ) *( OWS
+;;  "," [ OWS ( ( charset / "*" ) [ weight ] ) ] )
+;; Accept-Encoding = [ ( "," / ( codings [ weight ] ) ) *( OWS "," [ OWS
+;;  ( codings [ weight ] ) ] ) ]
+;; Accept-Language = *( "," OWS ) ( language-range [ weight ] ) *( OWS
+;;  "," [ OWS ( language-range [ weight ] ) ] )
+;; Allow = [ ( "," / method ) *( OWS "," [ OWS method ] ) ]
+
+;; BWS = <BWS, see [RFC7230], Section 3.2.3>
+
+;; Content-Encoding = *( "," OWS ) content-coding *( OWS "," [ OWS
+;;  content-coding ] )
+;; Content-Language = *( "," OWS ) language-tag *( OWS "," [ OWS
+;;  language-tag ] )
+;; Content-Location = absolute-URI / partial-URI
+;; Content-Type = media-type
+
+;; Date = HTTP-date
+
+;; Expect = "100-continue"
+
+;; From = mailbox
+
+;; GMT = %x47.4D.54 ; GMT
+
+;; HTTP-date = IMF-fixdate / obs-date
+
+;; IMF-fixdate = day-name "," SP date1 SP time-of-day SP GMT
+
+;; Location = URI-reference
+
+;; Max-Forwards = 1*DIGIT
+
+;; OWS = <OWS, see [RFC7230], Section 3.2.3>
+
+;; RWS = <RWS, see [RFC7230], Section 3.2.3>
+;; Referer = absolute-URI / partial-URI
+;; Retry-After = HTTP-date / delay-seconds
+
+;; Server = product *( RWS ( product / comment ) )
+
+;; URI-reference = <URI-reference, see [RFC7230], Section 2.7>
+;; User-Agent = product *( RWS ( product / comment ) )
+
+;; Vary = "*" / ( *( "," OWS ) field-name *( OWS "," [ OWS field-name ]
+;;  ) )
+
+;; absolute-URI = <absolute-URI, see [RFC7230], Section 2.7>
+
+;; accept-ext = OWS ";" OWS token [ "=" ( token / quoted-string ) ]
+(defn accept-ext []
+  (let [parser
+        (p/concat
+         (re/pattern-parser (re-pattern (re/re-concat OWS \; OWS)))
+         (optional-parameter))]
+    (fn [matcher]
+      (parser matcher))))
+
+;; qvalue = ( "0" [ "." *3DIGIT ] ) / ( "1" [ "." *3"0" ] )
+(def qvalue (re/re-compose "0(?:\\.[%s]{0,3})?|1(?:\\.[0]{0,3})?" DIGIT))
+
+;; weight = OWS ";" OWS "q=" qvalue
+(defn weight []
+  (let [pat (re-pattern (re/re-concat OWS \; OWS "q=" (re/group qvalue)))]
+    (fn [matcher]
+      (.usePattern matcher pat)
+      (when (.lookingAt matcher)
+        (Float/parseFloat (re/advance-and-return matcher (.group matcher 1)))))))
+
+;; accept-params = weight *accept-ext
+(defn accept-params []
+  (let [parser
+        (p/concat
+         (weight)
+         (p/sequence
+          (keep second)
+          (p/zero-or-more (accept-ext))))]
+    (fn [matcher]
+      (when-let [[weight accept-ext] (parser matcher)]
+        {:weight weight
+         :accept-ext (vec accept-ext)}))))
+
+
+(comment
+  ((accept-params) (re/input " ; q=0.3;foo=bar;zip")))
+
+;; asctime-date = day-name SP date3 SP time-of-day SP year
+
+;; charset = token
+;; codings = content-coding / "identity" / "*"
+;; comment = <comment, see [RFC7230], Section 3.2.6>
+;; content-coding = token
+
+;; date1 = day SP month SP year
+;; date2 = day "-" month "-" 2DIGIT
+;; date3 = month SP ( 2DIGIT / ( SP DIGIT ) )
+;; day = 2DIGIT
+;; day-name = %x4D.6F.6E ; Mon
+;;  / %x54.75.65 ; Tue
+;;  / %x57.65.64 ; Wed
+;;  / %x54.68.75 ; Thu
+;;  / %x46.72.69 ; Fri
+;;  / %x53.61.74 ; Sat
+;;  / %x53.75.6E ; Sun
+;; day-name-l = %x4D.6F.6E.64.61.79 ; Monday
+;;  / %x54.75.65.73.64.61.79 ; Tuesday
+;;  / %x57.65.64.6E.65.73.64.61.79 ; Wednesday
+;;  / %x54.68.75.72.73.64.61.79 ; Thursday
+;;  / %x46.72.69.64.61.79 ; Friday
+;;  / %x53.61.74.75.72.64.61.79 ; Saturday
+;;  / %x53.75.6E.64.61.79 ; Sunday
+;; delay-seconds = 1*DIGIT
+
+;; field-name = <comment, see [RFC7230], Section 3.2>
+
+;; hour = 2DIGIT
+
+;; language-range = <language-range, see [RFC4647], Section 2.1>
+;; language-tag = <Language-Tag, see [RFC5646], Section 2.1>
+
+;; mailbox = <mailbox, see [RFC5322], Section 3.4>
+
+;; quoted-string = <quoted-string, see [RFC7230], Section 3.2.6>
+
+;; parameter = token "=" ( token / quoted-string )
+
+;;(identity rfc7230/quoted-string)
+
+
+;; media-range = ( "*/*" / ( type "/*" ) / ( type "/" subtype ) ) *( OWS
+;;  ";" OWS parameter )
+(defn media-range
+  ([] (media-range {}))
+  ([opts]
+   (let [media-range-pattern
+         (re-pattern
+          (re/re-compose
+           "%s|%s|%s"
+           (str "(\\*)/(\\*)")
+           (format "(%s)/(\\*)" (re/re-str type))
+           (format "(%s)/(%s)" (re/re-str type) (re/re-str type))))
+
+         parameter-prefix
+         (re/pattern-parser (re-pattern (re/re-concat OWS \; OWS)))
+
+         parameters
+         (->> (p/zero-or-more
+               (p/concat
+                parameter-prefix
+                (parameter opts)))
+              (p/sequence (keep second)))]
+
+     (fn [matcher]
+       (.usePattern matcher media-range-pattern)
+       (when-let [result
+                  ((some-fn
+                    #(when-let [type (get % 1)]
+                       {:match (get % 0) :type type :subtype (get % 2)})
+                    #(when-let [type (get % 3)]
+                       {:match (get % 0) :type type :subtype (get % 4)})
+                    #(when-let [type (get % 5)]
+                       {:match (get % 0) :type type :subtype (get % 6)}))
+                   (re-find matcher))]
+
+         [:media-range
+          (conj
+           (re/advance-and-return matcher result)
+           [:params (parameters matcher)])])))))
+
+(comment
+  ((media-range)
+   (re/input "text/html ;  foo=bar ;  baz=\"qux;quuz\" ; q=0.9; a=b ; c")))
+
+;; media-type = type "/" subtype *( OWS ";" OWS parameter )
+
+;; method = token
+;; minute = 2DIGIT
+;; month = %x4A.61.6E ; Jan
+;;  / %x46.65.62 ; Feb
+;;  / %x4D.61.72 ; Mar
+;;  / %x41.70.72 ; Apr
+;;  / %x4D.61.79 ; May
+;;  / %x4A.75.6E ; Jun
+;;  / %x4A.75.6C ; Jul
+;;  / %x41.75.67 ; Aug
+;;  / %x53.65.70 ; Sep
+;;  / %x4F.63.74 ; Oct
+;;  / %x4E.6F.76 ; Nov
+;;  / %x44.65.63 ; Dec
+
+;; obs-date = rfc850-date / asctime-date
+
+;; partial-URI = <partial-URI, see [RFC7230], Section 2.7>
+;; product = token [ "/" product-version ]
+;; product-version = token
+
+;; rfc850-date = day-name-l "," SP date2 SP time-of-day SP GMT
+
+;; second = 2DIGIT
+
+;; time-of-day = hour ":" minute ":" second
+
+
+;; Accept = [ ( "," / ( media-range [ accept-params ] ) ) *( OWS "," [
+;;  OWS ( media-range [ accept-params ] ) ] ) ]
+
+(defn accept []
+  (let [media-range-pattern
+        (re-pattern
+         (re/re-compose
+          "%s|%s|%s"
+          (str "(\\*)/(\\*)")
+          (format "(%s)/(\\*)" (re/re-str type))
+          (format "(%s)/(%s)" (re/re-str type) (re/re-str type))))
+
+        media-range-parameter-prefix
+        (re/pattern-parser (re-pattern (re/re-concat OWS \; OWS)))
+
+        media-range-parameter-pattern
+        (p/concat media-range-parameter-prefix (parameter))
+
+        accept-params (accept-params)
+
+        ;; The reason why we can't just use `media-range` is that we
+        ;; need to resolve the ambiguity whereby the "q" parameter
+        ;; separates media type parameters from Accept extension
+        ;; parameters. This is more fully discussed in RFC 7231
+        ;; Section 5.3.2.
+        ;;
+        ;; The trick is to create a parameter parser modelled on
+        ;; `zero-or-more` which attempts to match `accept-params` for
+        ;; each parameter. Since `accept-params` matches on a leading
+        ;; `weight`, a weight parameter will be detected and cause the
+        ;; loop to end.
+        parameters
+        (fn this [matcher]
+          (loop [matcher matcher
+                 result {:parameters []}]
+            (if-let [accept-params (accept-params matcher)]
+              (merge result accept-params)
+              (if-let [match (media-range-parameter-pattern matcher)]
+                (recur matcher (update result :parameters conj (second match)))
+                result))))
+
+        media-range-with-accept-params-parser
+        (fn [matcher]
+          (when-let
+              [result
+               ((some-fn
+                 #(when-let [typ (get % 1)]
+                    {:type typ :subtype (get % 2)})
+                 #(when-let [typ (get % 3)]
+                    {:type typ :subtype (get % 4)})
+                 #(when-let [typ (get % 5)]
+                    {:type typ :subtype (get % 6)}))
+                (re/re-find-with-pattern matcher media-range-pattern))]
+              (into
+               {}
+               (concat
+                (re/advance-and-return matcher result)
+                (parameters matcher)))))]
+
+    (fn [matcher]
+      (let [parser
+            (p/sequence
+             (remove boolean?)
+             (p/cons
+              (p/comp
+               #(if (map? %) % true)
+               (some-fn
+                (re/pattern-parser #",")
+                media-range-with-accept-params-parser))
+              (p/zero-or-more
+               (p/comp
+                second
+                (p/concat
+                 (re/pattern-parser (re-pattern (re/re-concat OWS ",")))
+                 (p/optionally
+                  (p/comp
+                   second
+                   (p/concat
+                    (re/pattern-parser (re-pattern OWS))
+                    media-range-with-accept-params-parser))))))))]
+        (parser matcher)))))
+
+;; year = 4DIGIT
---
+++ test/juxt/reap/rfc7231_test.clj
@@ -0,0 +1,100 @@
+;; Copyright © 2020, JUXT LTD.
+
+(ns juxt.reap.rfc7231-test
+  (:require
+   [clojure.test :refer [deftest is testing]]
+   [juxt.reap.rfc7231 :as rfc7231]
+   [juxt.reap.regex :as re]))
+
+(deftest subtype-test
+  (is (= "html" (re-matches (re-pattern rfc7231/subtype) "html"))))
+
+(deftest parameter-matcher-test
+  (testing "value"
+    (is
+     (=
+      {:name "foo", :value "bar"}
+      ((rfc7231/parameter {})
+       (re/input "foo=bar"))))
+    (is
+     (nil?
+      ((rfc7231/parameter {})
+       (re/input "foo")))))
+
+  (testing "quoted-string"
+    (is
+     (=
+      {:name "foo", :value "ba'r" :raw-value "\"ba\\'r\""}
+      ((rfc7231/parameter)
+       (re/input "foo=\"ba\\'r\"")))))
+
+  (testing "optional parameter"
+    (is
+     (=
+      {:name "foo"}
+      ((rfc7231/optional-parameter)
+       (re/input "foo"))))
+    (is
+     (=
+      {:name "foo" :value "bar"}
+      ((rfc7231/optional-parameter)
+       (re/input "foo=bar"))))
+    (is
+     (=
+      {:name "foo", :value "ba'r" :raw-value "\"ba\\'r\""}
+      ((rfc7231/optional-parameter)
+       (re/input "foo=\"ba\\'r\""))))))
+
+(deftest media-range-test
+  (is
+   (=
+    [:media-range
+     {:match "text/html",
+      :type "text",
+      :subtype "html",
+      :params [{:name "foo", :value "bar"} {:name "baz", :value "qu'x" :raw-value "\"qu\\'x\""}]}]
+    ((rfc7231/media-range)
+     (re/input "text/html;foo=bar;baz=\"qu\\'x\"")))))
+
+
+(deftest qvalue-test
+  (is (re-matches (re-pattern rfc7231/qvalue) "1"))
+  (is (re-matches (re-pattern rfc7231/qvalue) "1.000"))
+  (is (re-matches (re-pattern rfc7231/qvalue) "0.9"))
+  (is (nil? (re-matches (re-pattern rfc7231/qvalue) "1.001")))
+  (is (nil? (re-matches (re-pattern rfc7231/qvalue) "1.0000")))
+  (is (nil? (re-matches (re-pattern rfc7231/qvalue) "0.1234"))))
+
+;; TODO: weight tests
+
+(deftest accept-test
+  (is
+   (=
+    [{:type "text",
+      :subtype "html",
+      :parameters [{:name "foo", :value "bar"}],
+      :weight (float 0.3)
+      :accept-ext [{:name "zip"} {:name "qux", :value "quik"}]}]
+    ((rfc7231/accept)
+     (re/input "text/html ;   foo=bar ;q=0.3;zip;\t qux=quik"))))
+
+  ;; https://www.newmediacampaigns.com/blog/browser-rest-http-accept-headers
+  (testing "Firefox"
+    (is
+     ((rfc7231/accept)
+      (re/input "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8"))))
+
+  (testing "Webkit"
+    (is
+     ((rfc7231/accept)
+      (re/input "application/xml,application/xhtml+xml,text/html;q=0.9,\r\ntext/plain;q=0.8,image/png,*/*;q=0.5"))))
+
+  (testing "IE"
+    (is
+     ((rfc7231/accept)
+      (re/input "image/jpeg, application/x-ms-application, image/gif,\r\napplication/xaml+xml, image/pjpeg, application/x-ms-xbap,\r\napplication/x-shockwave-flash, application/msword, */*"))))
+
+  (testing "Windows 7 Chrome"
+    (is
+     ((rfc7231/accept)
+      (re/input "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9")))))
