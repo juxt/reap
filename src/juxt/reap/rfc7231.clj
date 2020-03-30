19c741d124f138fbc956be5a6abfd1345c16bd9c
Malcolm Sparks <mal@juxt.pro>
2020-03-30 23:29:27 +0100

    as-map

--- src/juxt/reap/rfc7231.clj
+++ src/juxt/reap/rfc7231.clj
@@ -345,27 +345,49 @@
 
 ;; Accept-Charset = *( "," OWS ) ( ( charset / "*" ) [ weight ] ) *( OWS
 ;;  "," [ OWS ( ( charset / "*" ) [ weight ] ) ] )
+(let [parser
+      (let [charset-with-weight
+            (p/as-map
+             (p/sequence-group
+              (p/alternatives
+               (p/as-entry
+                :charset
+                (p/pattern-parser
+                 (re-pattern charset)))
+               (p/pattern-parser
+                (re-pattern (re/re-str \*))))
+              (p/optionally
+               (p/as-entry
+                :weight (weight)))))]
+        (p/cons
+         (p/first
+          (p/sequence-group
+           (p/ignore
+            (p/pattern-parser
+             (re-pattern (re/re-compose "(?:%s)*" (re/re-concat \, OWS)))))
+           charset-with-weight))
+         (p/zero-or-more
+          (p/first
+           (p/sequence-group
+            (p/ignore
+             (p/pattern-parser
+              (re-pattern (re/re-compose "%s%s" OWS ","))))
+            (p/optionally
+             (p/first
+              (p/sequence-group
+               (p/ignore
+                (p/pattern-parser
+                 (re-pattern OWS)))
+               charset-with-weight))))))))]
+  (criterium.core/quick-bench
+   (let [matcher (re/input ", \t, , , UTF-8;q=0.8,shift_JIS;q=0.4,a,b")]
+     (parser matcher))))
 
-(let [matcher (re/input ", \t, , , UTF-8;q=0.8,shift_JIS;q=0.4")]
-  (let [parser
-        (p/sequence-group
-         (p/ignore
-          (p/pattern-parser
-           (re-pattern (re/re-compose "(?:%s)*" (re/re-concat \, OWS)))
-           ))
-         (p/sequence-group
-          (p/alternatives
-           (p/pattern-parser
-            (re-pattern charset))
-           (p/pattern-parser
-            (re-pattern (re/re-str \*))))
-          (p/optionally (weight)
-                        )))]
-    (parser matcher)
-    ))
+(criterium.core/quick-bench
+   (let [matcher (re/input ", \t, , , UTF-8;q=0.8,shift_JIS;q=0.4,a,b")]
+     (parser matcher)))
 
-
-#_(require 'criterium.core)
+(require 'criterium.core)
 
 #_(let [p (accept)]
   (criterium.core/quick-bench
