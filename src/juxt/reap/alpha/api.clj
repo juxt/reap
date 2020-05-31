43b8ef3356f1d9accdf22fce38694ef57bce73a9
Malcolm Sparks <mal@juxt.pro>
2020-06-01 00:00:10 +0100

    Improve API
    Add fast accept

--- src/juxt/reap/alpha/api.clj
+++ src/juxt/reap/alpha/api.clj
@@ -2,6 +2,10 @@
 
 (ns juxt.reap.alpha.api
   (:require
-   [juxt.reap.alpha.rfc7231 :as rfc7231]))
+   [juxt.reap.alpha.rfc7231 :as rfc7231]
+   [juxt.reap.alpha.regex :as re]))
 
-(def accept rfc7231/accept)
+(def ^:private precompiled-accept (rfc7231/accept))
+
+(defn accept [s]
+  (precompiled-accept (re/input s)))
