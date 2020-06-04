3d374ae63fc8b67f878578e382d3c26d614c5961
Malcolm Sparks <mal@juxt.pro>
2020-06-04 15:40:34 +0100

    Add more precompiled functions

--- src/juxt/reap/alpha/api.clj
+++ src/juxt/reap/alpha/api.clj
@@ -9,3 +9,13 @@
 
 (defn accept [s]
   (precompiled-accept (re/input s)))
+
+(def ^:private precompiled-accept-language (rfc7231/accept-language))
+
+(defn accept-language [s]
+  (precompiled-accept-language (re/input s)))
+
+(def ^:private precompiled-content-type (rfc7231/content-type))
+
+(defn content-type [s]
+  (precompiled-content-type (re/input s)))
