bb1ad8cdb00eff48b0b4bca51991d7f094c70cb6
Malcolm Sparks <mal@juxt.pro>
2020-06-08 12:55:34 +0100

    Add parsing of language tags as per RFC 5646

---
+++ src/juxt/reap/alpha/rfc5646.clj
@@ -0,0 +1,121 @@
+;; Copyright © 2020, JUXT LTD.
+
+(ns juxt.reap.alpha.rfc5646
+  (:require
+   [clojure.string :as str]
+   [juxt.reap.alpha.combinators :as p]))
+
+(defn langtag []
+  (p/into
+   {:type :langtag}
+   (p/pattern-parser
+    (re-pattern
+     (str
+      "(?<Language>"
+      "(?<LangCode>\\p{Alpha}{2,3})(?=$|-)(?:-(?<ExtLang>\\p{Alpha}{3}(?:-\\p{Alpha}{3}){0,2}))?(?=$|-)"
+      "|"
+      "(?<Reserved>\\p{Alpha}{4})(?=$|-)"
+      "|"
+      "(?<RegLangSubtag>\\p{Alpha}{5,8})(?=$|-)"
+      ")"
+      ;; TODO: Try replacing positive lookahead with negative lookahead
+      "(?:\\-(?<Script>\\p{Alpha}{4}))?(?=$|-)"
+      "(?:\\-(?<Region>(\\p{Alpha}{2}|\\p{Digit}{3})))?(?=$|-)"
+
+      "(?:\\-(?<Variant>\\p{Alnum}{5,8}|\\p{Digit}\\p{Alnum}{3}))""*(?=$|-)"
+      "(?:\\-(?<Extension>[0-9A-WY-Za-wy-z](?:-\\p{Alnum}{2,8})+))*(?=$|-)"
+      "(?:\\-(?<PrivateUse>x-(?<PrivateTag>\\p{Alnum}{1,8}(?:-\\p{Alnum}{1,8})*)))?"
+      ;; Some negative lookahead to prevent premature matching
+      "(?!\\p{Alnum}|-)"
+      ))
+    {:group
+     {:langtag 0
+      :language "Language"
+      :code "LangCode"
+      :extlang "ExtLang"
+      :reserved "Reserved"
+      :reg-lang-subtag "RegLangSubtag"
+      :script "Script"
+      :region "Region"
+      :variant "Variant"
+      :extension "Extension"
+      :privateuse "PrivateUse"
+      :privatetag "PrivateTag"}})))
+
+(defn privateuse []
+  (p/into
+   {:type :privateuse}
+   (p/pattern-parser
+    #"x-(\p{Alnum}{1,8}(?:-\p{Alnum}{1,8})*)(?!\p{Alnum})"
+    {:group
+     {:langtag 0
+      :privatetag 1}})))
+
+(defn match-literal
+  ([set]
+   (match-literal set {}))
+  ([set opts]
+   (p/pattern-parser
+    (re-pattern
+     (str/join
+      "|"
+      (map
+       (fn [s]
+         (format
+          (str
+           "(?i:\\Q%s\\E)"
+           ;; We add some negative lookahead to avoid matching prefixes
+           "(?!\\p{Alpha})") s))
+       set)))
+    opts)))
+
+(defn irregular []
+  (p/into
+   {:subtype :irregular}
+   (match-literal
+    #{"en-GB-oed"
+      "i-ami"
+      "i-bnn"
+      "i-default"
+      "i-enochian"
+      "i-hak"
+      "i-klingon"
+      "i-lux"
+      "i-mingo"
+      "i-navajo"
+      "i-pwn"
+      "i-tao"
+      "i-tay"
+      "i-tsu"
+      "sgn-BE-FR"
+      "sgn-BE-NL"
+      "sgn-CH-DE"}
+    {:group {:langtag 0}})))
+
+(defn regular []
+  (p/into
+   {:subtype :regular}
+   (match-literal
+    #{"art-lojban"
+      "cel-gaulish"
+      "no-bok"
+      "no-nyn"
+      "zh-guoyu"
+      "zh-hakka"
+      "zh-min"
+      "zh-min-nan"
+      "zh-xiang"}
+    {:group {:langtag 0}})))
+
+(defn grandfathered []
+  (p/into
+   {:type :grandfathered}
+   (p/alternatives
+    (irregular)
+    (regular))))
+
+(defn language-tag []
+  (p/alternatives
+   (langtag)
+   (privateuse)
+   (grandfathered)))
