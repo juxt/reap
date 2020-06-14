02570ec1e68a8b1ab70b9baa3f3333aa4fa6a908
Malcolm Sparks <mal@juxt.pro>
2020-06-14 12:08:33 +0100

    Add accept-encoding to API

--- src/juxt/reap/alpha/api.clj
+++ src/juxt/reap/alpha/api.clj
@@ -15,6 +15,11 @@
 (defn accept-language [s]
   (precompiled-accept-language (re/input s)))
 
+(def ^:private precompiled-accept-encoding (rfc7231/accept-encoding))
+
+(defn accept-encoding [s]
+  (precompiled-accept-encoding (re/input s)))
+
 (def ^:private precompiled-content-type (rfc7231/content-type))
 
 (defn content-type [s]
