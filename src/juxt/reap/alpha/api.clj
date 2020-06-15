7cf686831a7c1874c78e4c95629d4ff971b45e2e
Malcolm Sparks <mal@juxt.pro>
2020-06-15 14:36:18 +0100

    Add API idempotent convenience functions
    Parsing can be expensive, so unnecessary parsing should be avoided where
    possible. These functions introduce a pattern to check for the argument being a
    string (and consequently, to be parsed).

--- src/juxt/reap/alpha/api.clj
+++ src/juxt/reap/alpha/api.clj
@@ -5,11 +5,18 @@
    [juxt.reap.alpha.rfc7231 :as rfc7231]
    [juxt.reap.alpha.regex :as re]))
 
+;; Accept
+
 (def ^:private precompiled-accept (rfc7231/accept))
 
 (defn accept [s]
   (when s
     (precompiled-accept (re/input s))))
+
+(defn accept-when-string [s]
+  (cond-> s (string? s) accept))
+
+;; Accept-Language
 
 (def ^:private precompiled-accept-language (rfc7231/accept-language))
 
@@ -17,11 +24,21 @@
   (when s
     (precompiled-accept-language (re/input s))))
 
+(defn accept-language-when-string [s]
+  (cond-> s (string? s) accept-language))
+
+;; Accept-Encoding
+
 (def ^:private precompiled-accept-encoding (rfc7231/accept-encoding))
 
 (defn accept-encoding [s]
   (when s
     (precompiled-accept-encoding (re/input s))))
+
+(defn accept-encoding-when-string [s]
+  (cond-> s (string? s) accept-encoding))
+
+;; Content-Type
 
 (def ^:private precompiled-content-type (rfc7231/content-type))
 
@@ -29,14 +46,27 @@
   (when s
     (precompiled-content-type (re/input s))))
 
+(defn content-type-when-string [s]
+  (cond-> s (string? s) content-type))
+
+;; Content-Language
+
 (def ^:private precompiled-content-language (rfc7231/content-language))
 
 (defn content-language [s]
   (when s
     (precompiled-content-language (re/input s))))
 
+(defn content-language-when-string [s]
+  (cond-> s (string? s) content-language))
+
+;; Content-Encoding
+
 (def ^:private precompiled-content-encoding (rfc7231/content-encoding))
 
 (defn content-encoding [s]
   (when s
     (precompiled-content-encoding (re/input s))))
+
+(defn content-encoding-when-string [s]
+  (cond-> s (string? s) content-encoding))
