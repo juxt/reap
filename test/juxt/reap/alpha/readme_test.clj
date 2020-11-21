894efbb9b0b6e285bd2a7c9325b456f3f56ab07a
Malcolm Sparks <mal@juxt.pro>
2020-11-21 17:49:32 +0000

    Use a more interesting example for the README
    This one contains parameters

--- test/juxt/reap/alpha/readme_test.clj
+++ test/juxt/reap/alpha/readme_test.clj
@@ -9,28 +9,37 @@
 (deftest readme-test
   (is
    (=
-    [#:juxt.reap.alpha.rfc7231
-     {:media-range "text/html",
-      :type "text",
-      :subtype "html",
-      :parameters {}}
-     #:juxt.reap.alpha.rfc7231
-     {:media-range "application/xhtml+xml",
-      :type "application",
-      :subtype "xhtml+xml",
-      :parameters {}}
-     #:juxt.reap.alpha.rfc7231
-     {:media-range "application/xml",
-      :type "application",
-      :subtype "xml",
-      :parameters {},
-      :qvalue 0.9}
-     #:juxt.reap.alpha.rfc7231
-     {:media-range "*/*",
-      :type "*",
-      :subtype "*",
-      :parameters {},
-      :qvalue 0.8}]
+    [#:juxt.reap.alpha.rfc7231{:media-range "text/html",
+                               :type "text",
+                               :subtype "html",
+                               :parameters {}}
+     #:juxt.reap.alpha.rfc7231{:media-range "application/xhtml+xml",
+                               :type "application",
+                               :subtype "xhtml+xml",
+                               :parameters {}}
+     #:juxt.reap.alpha.rfc7231{:media-range "application/xml",
+                               :type "application",
+                               :subtype "xml",
+                               :parameters {},
+                               :qvalue 0.9}
+     #:juxt.reap.alpha.rfc7231{:media-range "image/webp",
+                               :type "image",
+                               :subtype "webp",
+                               :parameters {}}
+     #:juxt.reap.alpha.rfc7231{:media-range "image/apng",
+                               :type "image",
+                               :subtype "apng",
+                               :parameters {}}
+     #:juxt.reap.alpha.rfc7231{:media-range "*/*",
+                               :type "*",
+                               :subtype "*",
+                               :parameters {},
+                               :qvalue 0.8}
+     #:juxt.reap.alpha.rfc7231{:media-range "application/signed-exchange",
+                               :type "application",
+                               :subtype "signed-exchange",
+                               :parameters {"v" "b3"},
+                               :qvalue 0.9}]
 
     (let [decoder (accept {})]
-      (decoder (re/input "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8"))))))
+      (decoder (re/input "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9"))))))
