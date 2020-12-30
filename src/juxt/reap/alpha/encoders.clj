c9b51cc3c0a2293a83e69fadf635d6e03d9c4abc
Malcolm Sparks <mal@juxt.pro>
2020-12-30 16:49:41 +0000

    Add encoder for content-range

--- src/juxt/reap/alpha/encoders.clj
+++ src/juxt/reap/alpha/encoders.clj
@@ -2,10 +2,16 @@
 
 (ns juxt.reap.alpha.encoders
   (:require
-   [juxt.reap.alpha.encoders.rfc7231 :as rfc7231]))
+   [juxt.reap.alpha.encoders.rfc7231 :as rfc7231]
+   [juxt.reap.alpha.encoders.rfc7233 :as rfc7233]))
 
 (def ^:private precompiled-http-date (rfc7231/http-date {}))
 
 (defn format-http-date [^java.util.Date inst]
   (assert (instance? java.util.Date inst) (format "Type is %s" (type inst)))
   (precompiled-http-date {:juxt.reap.alpha.rfc7231/date inst}))
+
+(def ^:private precompiled-content-range (rfc7233/content-range {}))
+
+(defn format-content-range [decoded]
+  (precompiled-content-range decoded))
