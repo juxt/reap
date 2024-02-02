f361494480a126319384ce4d5948de5b18c2237b
Malcolm Sparks <mal@juxt.pro>
2024-02-02 12:27:23 +0000

    Add tests for reserved expansion

--- test/juxt/reap/rfc6570_test.clj
+++ test/juxt/reap/rfc6570_test.clj
@@ -307,7 +307,34 @@
         "{keys}" "semi,%3B,dot,.,comma,%2C"
         "{keys*}" "semi=%3B,dot=.,comma=%2C")
 
-    ;; TODO: 3.2.3.  Reserved Expansion: {+var}
+    ;; 3.2.3.  Reserved Expansion: {+var}
+
+    (are [template expansion]
+        (= expansion (make-uri (compile-uri-template template) variables))
+
+        "{+var}" "value"
+        "{+hello}" "Hello%20World!"
+        "{+half}" "50%25"
+
+        "{base}index" "http%3A%2F%2Fexample.com%2Fhome%2Findex"
+        "{+base}index" "http://example.com/home/index"
+        "O{+empty}X" "OX"
+        "O{+undef}X" "OX"
+
+        "{+path}/here" "/foo/bar/here"
+        "here?ref={+path}" "here?ref=/foo/bar"
+        "up{+path}{var}/here" "up/foo/barvalue/here"
+        "{+x,hello,y}" "1024,Hello%20World!,768"
+        "{+path,x}/here" "/foo/bar,1024/here"
+
+        "{+path:6}/here" "/foo/b/here"
+        "{+list}" "red,green,blue"
+        "{+list*}" "red,green,blue"
+        "{+keys}" "semi,;,dot,.,comma,,"
+        "{+keys*}" "semi=;,dot=.,comma=,"
+
+        )
+
     ;; TODO: 3.2.4.  Fragment Expansion: {#var}
     ;; TODO: 3.2.5.  Label Expansion with Dot-Prefix: {.var}
     ;; TODO: 3.2.6.  Path Segment Expansion: {/var}
